a02d7aedd74a69707045c88efed5e3ca1b500954
jpiwoni <89944324+jpiwoni@users.noreply.github.com>
2023-02-05 20:52:46 -0600

    Add files via upload

---
+++ dice-roller.js
@@ -0,0 +1,18 @@
+function rollDice1() {
+  document.getElementById("dice1").value = Math.floor(Math.random()*6) + 1;
+}
+function rollDice2() {
+  document.getElementById("dice2").value = Math.floor(Math.random()*6) + 1;
+}
+function rollDice3() {
+  document.getElementById("dice3").value = Math.floor(Math.random()*6) + 1;
+}
+function rollDice4() {
+  document.getElementById("dice4").value = Math.floor(Math.random()*6) + 1;
+}
+function rollDice5() {
+  document.getElementById("dice5").value = Math.floor(Math.random()*6) + 1;
+}
+function rollDice6() {
+  document.getElementById("dice6").value = Math.floor(Math.random()*6) + 1;
+}
